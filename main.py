b4c0ff7920910e0e6f218313ef5e38be2a155340
Allan Pichardo <allan.pichardo@gmail.com>
2022-03-15 19:56:14 -0700

    infrastructure done

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+from datasets import GamesTrainingDataset, GamesValidationDataset
+from models import MatchPredictorModel
+import os
+import keras
+import tensorflow as tf
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+def main():
+    training_data = GamesTrainingDataset()
+    val_data = GamesValidationDataset()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    batch_size = 32
+    epochs = 100
+    version = '1'
+    log_dir = os.path.join(os.path.dirname(__file__), 'logs')
+    checkpoint_dir = os.path.join(os.path.dirname(__file__), 'checkpoints', version)
+    save_path = os.path.join(os.path.dirname(__file__), 'saved_models', version)
+
+    model = MatchPredictorModel()
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    else:
+        print("Found checkpoint. Loading weights")
+        model.load_weights(checkpoint_dir)
+
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+    callbacks = [
+        tf.keras.callbacks.EarlyStopping(patience=5),
+        tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_dir),
+        tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+    ]
+
+    training_data = training_data.shuffle(4000).cache().batch(batch_size)
+    val_data = val_data.shuffle(1000).cache().batch(batch_size)
+
+    model.fit(training_data, epochs=epochs, validation_data=val_data, callbacks=callbacks)
+
+    model.save(save_path, overwrite=True, include_optimizer=False)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
---
+++ models.py
@@ -0,0 +1,59 @@
+from abc import ABC
+
+import tensorflow as tf
+import keras
+from keras import layers
+
+from datasets import GamesValidationDataset
+
+
+class MatchPredictorModel(keras.Model, ABC):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.model = self._get_network()
+
+    def _get_conv_block(self, inputs):
+        a = layers.Conv1D(32, 7, padding='same', activation='relu')(inputs)
+        a = layers.MaxPool1D()(a)  # 36
+        a = layers.Conv1D(64, 5, padding='same', activation='relu')(a)
+        a = layers.MaxPool1D()(a)  # 18
+        a = layers.Conv1D(128, 3, padding='same', activation='relu')(a)
+        a = layers.GlobalAveragePooling1D()(a)  # 128
+        return a
+
+    def _get_network(self):
+        stats_a = layers.Input((36, 6))
+        home_a = layers.Input(())
+        stats_b = layers.Input((36, 6))
+        home_b = layers.Input(())
+
+        a = self._get_conv_block(stats_a)
+        b = self._get_conv_block(stats_b)
+
+        ha = layers.Add()([home_a, a])
+        hb = layers.Add()([home_b, b])
+
+        x = layers.Concatenate()([ha, hb])
+        x = layers.Dense(512, activation='relu')(x)
+        x = layers.Dense(2, activation='softmax')(x)
+
+        return keras.Model([stats_a, home_a, stats_b, home_b], x)
+
+    def call(self, inputs, training=None, mask=None):
+        stats_a = inputs['stats_1']
+        home_a = inputs['home_1']
+        stats_b = inputs['stats_2']
+        home_b = inputs['home_2']
+
+        return self.model([stats_a, home_a, stats_b, home_b])
+
+
+if __name__ == '__main__':
+    model = MatchPredictorModel()
+    dataset = GamesValidationDataset()
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy')
+
+    model.fit(dataset.batch(8))
